70e719c40486a3e6d9cee727ca5b6e06a204b416
Dahyeon Jin <wlsekgus12@gmail.com>
2024-08-09 19:27:52 +0900

    Chore: Build 테스트 관련 세팅값 조정 취소

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,10 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import path from 'path';
 
 // Vite 설정
 export default defineConfig({
   plugins: [react()],
   build: {
     sourcemap: true, // production에서도 Sourcemap 활성화
-    rollupOptions: {
-      input: 'src/main.tsx',
-      output: {
-        preserveModules: true,
-        entryFileNames: '[name].js',
-        dir: 'dist',
-      },
-    },
   },
 });
