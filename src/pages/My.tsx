75ed5f28fa7623bb9240dccf864fd825c05493d3
Dahyeon Jin <wlsekgus12@gmail.com>
2024-08-09 12:41:48 +0900

    Style: CSS 보완

--- src/pages/My.tsx
+++ src/pages/My.tsx
@@ -38,7 +38,7 @@ const My: React.FC = () => {
   };
 
   return (
-    <div className='p-4'>
+    <div className='px-10'>
       <div className='mb-3'>
         <label className='block font-medium'>닉네임</label>
         <input
@@ -68,7 +68,7 @@ const My: React.FC = () => {
       </div>
       <button
         onClick={handleUpdate}
-        className='bg-blue-500 text-white px-4 py-2 rounded'
+        className='bg-blue-500 text-white px-4 py-2 mt-2 rounded'
       >
         프로필 변경하기
       </button>
