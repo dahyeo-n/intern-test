9a8556f184915f6c817500b00810163a91e0f03f
Dahyeon Jin <wlsekgus12@gmail.com>
2024-08-09 16:16:07 +0900

    Chore: Sentry 설치 및 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import * as Sentry from '@sentry/react';
+
+// Sentry 초기화
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+});
 
 export const queryclient = new QueryClient();
 
