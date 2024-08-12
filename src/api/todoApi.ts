71348b2135525ea9877013816f69a4b42793f659
Dahyeon Jin <wlsekgus12@gmail.com>
2024-08-12 23:25:46 +0900

    Fix: Todo 타입 에러 해결

--- src/api/todoApi.ts
+++ src/api/todoApi.ts
@@ -1,11 +1,14 @@
+import { Todo } from '../types';
 import axiosTodosInstance from './axios';
 
-export const fetchTodos = async (): Promise<any[]> => {
+export const fetchTodos = async (): Promise<Todo[]> => {
   const response = await axiosTodosInstance.get('/todos');
+  console.log('Todos => ', response.data);
   return response.data;
 };
 
-export const fetchTodo = async (id: number): Promise<any> => {
+export const fetchTodo = async (id: number): Promise<Todo> => {
   const response = await axiosTodosInstance.get(`/todos/${id}`);
+  console.log('Todo => ', response.data);
   return response.data;
 };
